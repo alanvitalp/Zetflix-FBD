340238114629fa132271028b1f000b3107bd6cc2
Alan Vital <49616136+alanvitalp@users.noreply.github.com>
2021-04-12 15:28:18 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "aluno",
-  password: "Ck0114",
-  host: "200.129.44.249",
+  user: "",
+  password: "",
+  host: "",
   port: 5432,
-  database: "rene_473951"
+  database: ""
 });
 
 module.exports = pool;
